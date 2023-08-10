d1014b71ba4b716fe4876a19478df590a2eec917
zinzoddari <zinzoddari@gmail.com>
2023-08-10 23:52:36 +0900

    first init

---
+++ src/main/java/com/moiming/MoimingApplication.java
@@ -0,0 +1,13 @@
+package com.moiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoimingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moiming/MoimingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moiming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
