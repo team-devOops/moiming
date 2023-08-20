d1354edd97be6b80954dfb6f153ead351f0e7efd
zinzoddari <zinzoddari@gmail.com>
2023-08-21 00:54:37 +0900

    feat: 모임 조회 관련 추가

--- src/main/java/com/moiming/meet/web/MeetsController.java
+++ src/main/java/com/moiming/meet/web/MeetsController.java
@@ -2,12 +2,26 @@ package com.moiming.meet.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/meets")
 @Tag(name = "모임 관리", description = "모임 관리 관련 API")
 public class MeetsController {
+
+    @GetMapping
+    @Operation(summary = "모임 조회", description = "생성되어 있는 모임 리스트를 조회합니다.")
+    public void meetSelect() {
+        //TODO: 리스트 조회의 조건 정책 필요
+    }
+
+    @GetMapping("/{meetSeq}")
+    @Operation(summary = "모임 상세 조회", description = "특정 모임을 상세 조회합니다.")
+    public void meetSelect(@PathParam("meetSeq") Long meetSeq) {
+        //TODO: meetSeq에 대한 모임 상세 정보를 조회합니다.
+    }
+
     @PostMapping
     @Operation(summary = "모임 생성", description = "모임을 생성합니다.")
     public void meetCreate() {
