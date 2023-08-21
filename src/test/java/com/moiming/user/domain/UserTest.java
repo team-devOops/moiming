21bb0796709e700e2488fe63fb8bb7546d96f7e8
benggrae <kbh0581@gmail.com>
2023-08-22 00:21:51 +0900

    style: 어노테이션 순서 변경

--- src/test/java/com/moiming/user/domain/UserTest.java
+++ src/test/java/com/moiming/user/domain/UserTest.java
@@ -18,40 +18,41 @@ class UserTest {
     void setUp() {
         collectEmail = Email.of("kbh058@naver.com");
     }
-    @DisplayName("유저의 이름은 비어 있으면 안된다")
-    @NullAndEmptySource
+
     @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("유저의 이름은 비어 있으면 안된다")
     void noNameUser(String name) {
         assertThatIllegalArgumentException().isThrownBy(() ->
                 User.createUser("kbh052", collectEmail, name, "1234", LocalDate.of(1995, 8, 2)));
 
     }
 
-    @DisplayName("유저의 아이디는 비어 있으면 안된다")
-    @NullAndEmptySource
     @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("유저의 아이디는 비어 있으면 안된다")
     void noIdUser(String id) {
         assertThatIllegalArgumentException().isThrownBy(()
                 -> User.createUser(id, collectEmail, "name", "1234", LocalDate.of(1995, 8, 2)));
     }
 
-    @DisplayName("유저의 비밀번호는 비어 있으면 안된다")
-    @NullAndEmptySource
     @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("유저의 비밀번호는 비어 있으면 안된다")
     void noPasswordUser(String password) {
         assertThatIllegalArgumentException().isThrownBy(()
                 -> User.createUser("kbh052", collectEmail, "name", password, LocalDate.of(1995, 8, 2)));
     }
 
-    @DisplayName("유저의 생일은 비어 있으면 안된다")
     @Test
+    @DisplayName("유저의 생일은 비어 있으면 안된다")
     void noBirthDateUser() {
         assertThatIllegalArgumentException().isThrownBy(()
                 -> User.createUser("kbh052", collectEmail, "name", "1234", null));
     }
 
-    @DisplayName("유저의 이메일은 비어 있으면 안된다")
     @Test
+    @DisplayName("유저의 이메일은 비어 있으면 안된다")
     void noEmailUser() {
         assertThatIllegalArgumentException().isThrownBy(()
                 -> User.createUser("kbh052", (Email) null, "name", "1234", LocalDate.now()));
