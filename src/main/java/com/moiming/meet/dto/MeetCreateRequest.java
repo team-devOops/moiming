acbaecdd71c7fb3655d1e21ff93938c55de104f8
Zinzo <42106799+zinzoddari@users.noreply.github.com>
2023-08-24 01:35:21 +0900

    docs: gradle.yml 생성 (#13)
    * docs: gradle.yml
    
    * fix: build오류 수정
    
    * docs: PR시 테스트할 수 있도록 추가
    
    * refactor

--- src/main/java/com/moiming/meet/dto/MeetCreateRequest.java
+++ src/main/java/com/moiming/meet/dto/MeetCreateRequest.java
@@ -4,8 +4,7 @@ import com.moiming.meet.domain.MeetInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDate;
 
