856826ba195e4d1cd0b23d64362666b28eaf08d3
zinzoddari <zinzoddari@gmail.com>
2023-08-24 00:02:07 +0900

    docs: PR시 테스트할 수 있도록 추가

--- src/main/java/com/moiming/meet/dto/MeetCreateRequest.java
+++ src/main/java/com/moiming/meet/dto/MeetCreateRequest.java
@@ -4,7 +4,6 @@ import com.moiming.meet.domain.MeetInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.*;
 
 import java.time.LocalDate;
 
